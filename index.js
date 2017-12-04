ddb82a9a50640b924705d979f6e50c521d9fe140
John Britzke <thaumaturgus@users.noreply.github.com>
2017-12-04 01:52:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// --- Directions
-// Create an 'eventing' library out of the
-// Events class.  The Events class should
-// have methods 'on', 'trigger', and 'off'.
-
 class Events {
   constructor() {
     this.events = {};
